d4cbaee219b38d24ea93038deef36da184cabb5a
8590priya <83853581+8590priya@users.noreply.github.com>
2021-12-30 12:55:40 +0530

    Add files via upload

---
+++ AddTointeger.java
@@ -0,0 +1,12 @@
+
+public class AddTointeger {
+	public static void main(String[] args) {
+		int num1=1;
+				int num2=2;
+				int sum=num1+num2;
+				System.out.println("Sum="+sum);
+		
+		
+	}
+
+}
